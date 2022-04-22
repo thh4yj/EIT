993182d7230d013ca0aa549389851c5b3b35b2c7
thh4yj <thh4yj@virginia.edu>
2022-04-22 12:16:33 -0400

    Fixed error where nextState switch wouldn't run. Using If/else now.

--- FSM.ino
+++ FSM.ino
@@ -4,69 +4,90 @@ void initFSM(){
     
 }
 
+#define minTemp 100
+#define maxTemp 120
 FSMState nextStateFunction(FSMState currentState){
-    FSMState nextState;
-    switch(currentState){
-        case wait:
-            if(imuFlag){
-                nextState = activate;
-            }
-            else{
-                nextState = wait;
-            }
-        break;
-        case activate:
-            //TODO add function to poll temperature
-            bool temperature = 0;
-            if(temperature < 100){
-                nextState = tooCold;
-            }
-            else if(temperature <= 120 && temperature >= 100){
-                nextState = justRight;
-            }
-            else if(temperature > 120){
-                nextState = tooHot;
-            }
-            else{ //an error occured
-                nextState = error;
-            }
-            break;
-        case tooCold:
+    Serial.print("Current state: ");
+    Serial.println(currentState);
+    FSMState nextState = error;
+    Serial.println("switch");
+    if(currentState == wait){
+        Serial.println("Wait state");
+        if(imuFlag){
+            nextState = activate;
+        }
+        else{
             nextState = wait;
-            break;
-        case tooHot:
-            nextState = wait;
-            break;
-        case justRight:
-            nextState = wait;
-            break;
-        case error:
-            nextState = wait;
-            break;
+        }
     }
+    else if(currentState ==  activate){
+        Serial.println("Act state");
+        if(temperature < minTemp){
+            nextState = tooCold;
+        }
+        else if(temperature <= maxTemp && temperature >= minTemp){
+            nextState = justRight;
+        }
+        else if(temperature > maxTemp){
+            nextState = tooHot;
+        }
+        else{ //an error occured
+            Serial.println("Changing state to error");
+            nextState = error;
+        }
+    }
+    else if(currentState ==  tooCold){
+        Serial.println("cold state");
+        nextState = wait;
+    }
+    else if(currentState ==  tooHot){
+        Serial.println("hot state");
+        nextState = wait;
+    }
+    else if(currentState ==  justRight){
+        Serial.println("right state");
+        nextState = wait;
+    }
+    else if(currentState ==  error){
+        Serial.println("Next state wait from error!");
+        nextState = wait;
+    }
+    else{
+        Serial.println("Default reached, erro!");
+    }
+    Serial.print("Next state: ");
+    Serial.println(nextState);
     return nextState;
 }
+
 
 void outputStateFunction(FSMState currentState){
     switch (currentState)
     {
     case wait:
         //TODO add code to check IMU and set flag
+        imuFlag = 1;
+        Serial.println("wait");
         break;
     case activate:
         int data = readTo();
         temperature = handleTo(data);
+        Serial.println(temperature);
         break;
     case tooCold:
+        Serial.println("Pulse cold");
         pulseCold();
         break;
     case tooHot:
+        Serial.println("Pulse hot");
         pulseHot();
         break;
     case justRight:
+        Serial.println("Pulse right");
         pulseRight();
         break;
     case error:
+        Serial.println("Pulse error");
         pulseError();
         break;
     }
